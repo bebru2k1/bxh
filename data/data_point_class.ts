17dffb0fbc2856ae54fbf1354b33841eea9f2ef4
bebru2k1 <ducanhlaze2k1@gmail.com>
2023-03-01 00:48:20 +0700

    update points

--- data/data_point_class.ts
+++ data/data_point_class.ts
@@ -1,24 +1,193 @@
 const avatar = 'https://wdwicpictures.com/hinh-anh-sieu-nhan-gao-do/imager_1_4674_700.jpg'
 
 export const pointsA03 = [
+    // {
+    //     points: 10,
+    //     display_name: "Nhật",
+    //     avatar
+    // },
+    // {
+    //     points: 20,
+    //     display_name: "Tuấn",
+    //     avatar
+    // },
+    // {
+    //     points: 10,
+    //     display_name: "Dương",
+    //     avatar
+    // },
     {
         points: 10,
-        display_name: "Nhật",
+        display_name: "Thúy",
         avatar
     },
     {
-        points: 20,
+        points: 10,
+        display_name: "Khoa",
+        avatar
+    },
+    {
+        points: 10 + 20,
+        display_name: "Hưng",
+        avatar
+    },
+    {
+        points: 10 + 20 + 10,
+        display_name: "Anh Tuấn",
+        avatar
+    },{
+        points: 10,
+        display_name: "Duy",
+        avatar
+    },{
+        points: 10,
         display_name: "Tuấn",
         avatar
-    },
-    {
+    },{
         points: 10,
+        display_name: "Ban",
+        avatar
+    },{
+        points: 10,
+        display_name: "Quân",
+        avatar
+    },{
+        points: 10,
+        display_name: "Phúc",
+        avatar
+    },{
+        points: 9,
+        display_name: "Vy",
+        avatar
+    },{
+        points: 9,
+        display_name: "C. Tú",
+        avatar
+    },{
+        points: 9,
+        display_name: "Ngọc",
+        avatar
+    },{
+        points: 9,
+        display_name: "Nguyên",
+        avatar
+    },{
+        points: 9 + 10,
+        display_name: "Nhật",
+        avatar
+    },{
+        points: 9,
+        display_name: "V.Hiếu",
+        avatar
+    },{
+        points: 9,
+        display_name: "Ánh",
+        avatar
+    },{
+        points: 9,
+        display_name: "Phong",
+        avatar
+    },{
+        points: 9,
+        display_name: "Phương",
+        avatar
+    },{
+        points: 10 + 10,
         display_name: "Dương",
         avatar
-    }]
+    },{
+        points: 10,
+        display_name: "Thiết",
+        avatar
+    },{
+        points: 10,
+        display_name: "Thịnh",
+        avatar
+    },{
+        points: 10,
+        display_name: "Q.Anh",
+        avatar
+    },{
+        points: 10,
+        display_name: "G.Bảo",
+        avatar
+    },{
+        points: 10,
+        display_name: "Hiền",
+        avatar
+    },{
+        points: 10,
+        display_name: "N. Tú",
+        avatar
+    },{
+        points: 10,
+        display_name: "Thắng",
+        avatar
+    },{
+        points: 10,
+        display_name: "Minh Hiếu",
+        avatar
+    },{
+        points: 10,
+        display_name: "Thùy",
+        avatar
+    },
+    {
+        points: 9,
+        display_name: "Trang",
+        avatar
+    },{
+        points: 9,
+        display_name: "Na",
+        avatar
+    },{
+        points: 9,
+        display_name: "Giang",
+        avatar
+    },{
+        points: 9,
+        display_name: "Chi",
+        avatar
+    },{
+        points: 9,
+        display_name: "Hải",
+        avatar
+    },{
+        points: 9,
+        display_name: "Quốc Anh",
+        avatar
+    },{
+        points: 9,
+        display_name: "Toàn",
+        avatar
+    },{
+        points: 9,
+        display_name: "Q. Bảo",
+        avatar
+    },{
+        points: 9 + 20,
+        display_name: "Tùng",
+        avatar
+    },{
+        points: 9,
+        display_name: "Đảo",
+        avatar
+    }
+]
 
 export const pointsD02 = [{
-    points: 20,
+    points: 20 + 10,
     display_name: "Tài",
     avatar
-}]
+},
+{
+    points: 20 + 10,
+    display_name: "Mai",
+    avatar
+},
+{
+    points: 10,
+    display_name: "Trâm Anh",
+    avatar
+}
+]
